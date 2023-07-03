3acc06f3fd89dbc4bae0b7bdd205255b62f142e9
LucasAb0y <126517371+LucasAb0y@users.noreply.github.com>
2023-07-03 03:40:53 -0300

    trabajo practico n2

--- style.js
+++ style.js
@@ -1,15 +1,39 @@
-let cantidad
-let precio
-let desc
-let compra
-let pagar
 
-precio = parseFloat(prompt("Ingresar Precio"));
-cantidad = parseFloat(prompt("Ingresar Cantidad"));
-
-compra = precio * cantidad ;
-desc = compra * 0.20;
-pagar = compra-desc;
-
-console.log("El descuento es:"+desc);
-console.log("El Monto a pagar es:"+pagar);
+const auto1 = { 
+    modelo: 'nissan',
+    color: 'azul',
+    peso: 1500,
+    vender: function(){
+        return `El ${auto1.modelo} fue vendido el día ${new Date().toLocaleDateString()}`;
+    }
+ };
+ 
+ const auto2 = { 
+    modelo: 'Audi',
+    color: 'blanco',
+    peso: 1700,
+    vender: function(){
+        return `El ${auto2.modelo} fue vendido el día ${new Date().toLocaleDateString()}`
+    }
+ };
+ 
+ const auto3 = { 
+    modelo: 'Mazda',
+    color: 'rojo',
+    peso: 1800,
+    vender: function(){
+        return `El ${auto3.modelo} fue vendido el día ${new Date().toLocaleDateString()}`
+    }
+ };
+ 
+ const numeroAuto = prompt('Ingrese el número del auto para saber:');
+ if (numeroAuto === '1') {
+    console.log(auto1.vender());
+ } else if (numeroAuto === '2') {
+    console.log(auto2.vender());
+ } else if (numeroAuto === '3') {
+    console.log(auto3.vender());
+ } else {
+    console.log('Número de auto inválido.');
+ }
+   
