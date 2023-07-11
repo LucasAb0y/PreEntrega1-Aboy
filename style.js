9a6b0eb4820df9345cc31d5ba57b966fb77535e3
LucasAb0y <126517371+LucasAb0y@users.noreply.github.com>
2023-07-10 21:02:39 -0300

    arreglo del tp

--- style.js
+++ style.js
@@ -1,39 +1,36 @@
-
-const auto1 = { 
-    modelo: 'nissan',
-    color: 'azul',
-    peso: 1500,
-    vender: function(){
-        return `El ${auto1.modelo} fue vendido el día ${new Date().toLocaleDateString()}`;
-    }
- };
+const autos = [
+   {
+     modelo: 'nissan',
+     color: 'azul',
+     peso: 1500,
+     vender: function() {
+       return `El ${this.modelo} fue vendido el día ${new Date().toLocaleDateString()}`;
+     }
+   },
+   {
+     modelo: 'Audi',
+     color: 'blanco',
+     peso: 1700,
+     vender: function() {
+       return `El ${this.modelo} fue vendido el día ${new Date().toLocaleDateString()}`;
+     }
+   },
+   {
+     modelo: 'Mazda',
+     color: 'rojo',
+     peso: 1800,
+     vender: function() {
+       return `El ${this.modelo} fue vendido el día ${new Date().toLocaleDateString()}`;
+     }
+   }
+ ];
  
- const auto2 = { 
-    modelo: 'Audi',
-    color: 'blanco',
-    peso: 1700,
-    vender: function(){
-        return `El ${auto2.modelo} fue vendido el día ${new Date().toLocaleDateString()}`
-    }
- };
+ const numeroAuto = prompt('Ingrese el número 1,2,3 para saber el auto :');
  
- const auto3 = { 
-    modelo: 'Mazda',
-    color: 'rojo',
-    peso: 1800,
-    vender: function(){
-        return `El ${auto3.modelo} fue vendido el día ${new Date().toLocaleDateString()}`
-    }
- };
+ const autoEncontrado = autos.find((auto, index) => (index + 1).toString() === numeroAuto);
  
- const numeroAuto = prompt('Ingrese el número del auto para saber:');
- if (numeroAuto === '1') {
-    console.log(auto1.vender());
- } else if (numeroAuto === '2') {
-    console.log(auto2.vender());
- } else if (numeroAuto === '3') {
-    console.log(auto3.vender());
+ if (autoEncontrado) {
+   console.log(autoEncontrado.vender());
  } else {
-    console.log('Número de auto inválido.');
+   console.log('Número de auto inválido.');
  }
-   
